f7658b9ffc0e8152ad6cb14e7e6f0458a002408b
Dylan Lyu <mingzong.lyu@gmail.com>
2020-09-08 12:31:09 +0800

    [Change] fulfill user repository

--- internal/repository/user/user.go
+++ internal/repository/user/user.go
@@ -32,6 +32,45 @@ func (ur userRepo) Create(user *models.User) (*models.User, error) {
 	return user,nil
 }
 
-func (ur userRepo) Read() (users []*models.User, err error) {
+func (ur userRepo) Read() ([]*models.User,error) {
+	var users []*models.User
+	err := ur.db.Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users,nil
+}
 
+func (ur userRepo) ReadByID(id int) (*models.User,error) {
+	var user *models.User
+	err := ur.db.First(&user,id).Error
+	if err != nil {
+		return nil, err
+	}
+	return user,nil
+}
+
+func (ur userRepo) ReadByName(name string) ([]*models.User,error) {
+	var users []*models.User
+	err := ur.db.Find(&users,"name LINK ?","%"+name+"%").Error
+	if err != nil {
+		return nil, err
+	}
+	return users,nil
+}
+
+func (ur userRepo) Update(user *models.User) (*models.User,error) {
+	err := ur.db.Save(&user).Error
+	if err != nil {
+		return nil, err
+	}
+	return user,err
+}
+
+func (ur userRepo) Delete(user *models.User) (*models.User,error) {
+	er := ur.db.Delete(&user).Error
+	if er != nil {
+		return nil, er
+	}
+	return user,nil
 }
